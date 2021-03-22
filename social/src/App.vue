c78bcf67a2f2bcd134abb1df3528497c736e3dfa
davilopess <davi.lopes.amorim2014@hotmail.com>
2021-03-22 11:12:29 -0300

    First commit

---
+++ social/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <span>
+    <header>
+      <nav-bar cor="blue darken-2" logo="Social" url="#"/>
+    </header>
+
+    <main>
+      <div class="container">
+        <router-view /> 
+
+      </div>
+    </main>
+
+    <footer-vue/>
+
+  </span>
+</template>
+
+<script>
+import NavBar from '@/components/layouts/NavBar'
+import FooterVue from '@/components/layouts/FooterVue'
+export default {
+  name: "App",
+  components: {
+    NavBar,
+    FooterVue
+  }
+};
+</script>
+
+<style>
+</style>
