d5439104198d8636b356f337e34c621d07772f4c
davilopess <davi.lopes.amorim2014@hotmail.com>
2021-03-22 12:42:56 -0300

    [feat] Criando componente de grid

--- social/src/App.vue
+++ social/src/App.vue
@@ -1,30 +1,43 @@
 <template>
   <span>
     <header>
-      <nav-bar cor="blue darken-2" logo="Social" url="#"/>
+      <nav-bar cor="blue darken-2" logo="Social" url="#" />
     </header>
 
     <main>
       <div class="container">
-        <router-view /> 
-
+        <div class="row">
+          <grid-vue tamanho="4">
+            Menu
+          </grid-vue>
+          <grid-vue tamanho="8">
+            <router-view />
+          </grid-vue>
+        </div>
+        
       </div>
     </main>
 
-    <footer-vue/>
-
+    <footer-vue cor="blue darken-2 " logo="Social" descricao="Teste">
+      <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+    </footer-vue>
   </span>
 </template>
 
 <script>
-import NavBar from '@/components/layouts/NavBar'
-import FooterVue from '@/components/layouts/FooterVue'
+import NavBar from "@/components/layouts/NavBar";
+import FooterVue from "@/components/layouts/FooterVue";
+import GridVue from "@/components/layouts/GridVue";
 export default {
   name: "App",
   components: {
     NavBar,
-    FooterVue
-  }
+    FooterVue,
+    GridVue,
+  },
 };
 </script>
 
