ad7e47cf8d5697067361758f3e9c922eaba98036
davilopess <davi.lopes.amorim2014@hotmail.com>
2021-03-22 14:12:30 -0300

    [feat] Adicionando conteudo

--- social/src/App.vue
+++ social/src/App.vue
@@ -8,13 +8,31 @@
       <div class="container">
         <div class="row">
           <grid-vue tamanho="4">
-            Menu
+            <card-menu-vue>
+              <div class="row valign-wrapper">
+                <grid-vue tamanho="4">
+                  <img
+                    src="http://materializecss.com/images/yuna.jpg"
+                    alt=""
+                    class="circle responsive-img"
+                  />
+                  <!-- notice the "circle" class -->
+                </grid-vue>
+                <grid-vue tamanho="8">
+                  <span class="black-text">
+                    <h5>Maria</h5>
+                    This is a square image. Add the "circle" class to it to make
+                    it appear circular.
+                  </span>
+                </grid-vue>
+              </div>
+            </card-menu-vue>
+            
           </grid-vue>
           <grid-vue tamanho="8">
             <router-view />
           </grid-vue>
         </div>
-        
       </div>
     </main>
 
@@ -31,12 +49,14 @@
 import NavBar from "@/components/layouts/NavBar";
 import FooterVue from "@/components/layouts/FooterVue";
 import GridVue from "@/components/layouts/GridVue";
+import CardMenuVue from "@/components/layouts/CardMenuVue";
 export default {
   name: "App",
   components: {
     NavBar,
     FooterVue,
     GridVue,
+    CardMenuVue
   },
 };
 </script>
---
+++ social/src/components/social/CardConteudoVue.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div class="row valign-wrapper">
+        <grid-vue tamanho="4">
+          <img
+            src="http://materializecss.com/images/yuna.jpg"
+            alt=""
+            class="circle responsive-img"
+          />
+          <!-- notice the "circle" class -->
+        </grid-vue>
+        <grid-vue tamanho="8">
+          <span class="black-text">
+            <h5>Maria</h5>
+            This is a square image. Add the "circle" class to it to make it
+            appear circular.
+          </span>
+        </grid-vue>
+      </div>
+    </div>
+    <div class="card-action">
+      <a href="#">This is a link</a>
+      <a href="#">This is a link</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import GridVue from "@/components/layouts/GridVue";
+export default {
+  name: "CardConteudoVue",
+  props: [],
+  data() {
+    return {};
+  },
+  components: {
+    GridVue,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
