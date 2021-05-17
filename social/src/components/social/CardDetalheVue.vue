134b7c4cf9da7dc6314021dec2ab02522ff91a3f
davilopess <davi.lopes.amorim2014@hotmail.com>
2021-05-16 23:18:29 -0300

    [feat] Criando espaço para publicar conteudo

---
+++ social/src/components/social/CardDetalheVue.vue
@@ -0,0 +1,32 @@
+<template>
+<span>
+
+     <div v-if="img" class="card-image">
+      <img :src="img" />
+      <span class="card-title">{{titulo || ''}}</span>
+    </div>
+
+    <div class="card-content">
+      <p>
+        {{txt}}
+      </p>
+    </div>
+</span>
+</template>
+
+<script>
+
+export default {
+  name: "CardDetalheVue",
+  props: ['img', 'titulo', 'txt'],
+  data() {
+    return {};
+  },
+  components: {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -1,26 +1,40 @@
 <template>
+
+<span>
+
+  <div class="row">
+        <grid-vue class="input-field" tamanho="12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label for="textarea1">Textarea</label>
+        </grid-vue>
+
+        <p>
+          <grid-vue class="btn waves-effect waves-light" tamanho="2 offset-s10">Publicar</grid-vue>
+        </p>
+  </div>
+
   <card-conteudo-vue
     perfil="http://materializecss.com/images/yuna.jpg"
     nome="Maria"
     data="12/08/2021 13:00"
   >
-    <div class="card-image">
-      <img src="http://materializecss.com/images/sample-1.jpg" />
-      <span class="card-title">Card Title</span>
-    </div>
 
-    <div class="card-content">
-      <p>
-        I am a very simple card. I am good at containing small bits of
+    <card-detalhe-vue 
+      img="http://materializecss.com/images/sample-1.jpg" 
+      titulo = ""
+      txt="I am a very simple card. I am good at containing small bits of
         information. I am convenient because I require little markup to use
-        effectively.
-      </p>
-    </div>
+        effectively."      
+      />
   </card-conteudo-vue>
+</span>
 </template>
 
 <script>
 import CardConteudoVue from "@/components/social/CardConteudoVue";
+import CardDetalheVue from "@/components/social/CardDetalheVue";
+import GridVue from "@/components/layouts/GridVue";
+
 export default {
   name: "Home",
   data() {
@@ -28,10 +42,13 @@ export default {
   },
   components: {
     CardConteudoVue,
+    CardDetalheVue,
+    GridVue
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-
+    CardConteudoVue- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
